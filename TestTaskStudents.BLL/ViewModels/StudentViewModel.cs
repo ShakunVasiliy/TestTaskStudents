b1f735009ca1da6f2e6d9f96a7b92bf917157f1a
Vasiliy Shakun <shakun.vasiliy@gmail.com>
2017-08-05 13:07:03 +0300

    Added  class StudentViewModel

---
+++ TestTaskStudents.BLL/ViewModels/StudentViewModel.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+using TestTaskStudents.BLL.Infrastructure;
+using TestTaskStudents.DAL.Models;
+using TestTaskStudents.DAL.Infrastructure;
+
+namespace TestTaskStudents.BLL.ViewModels
+{
+    public class StudentViewModel : ChangeNotifier
+    {
+        private Student student;
+
+        public StudentViewModel(Student student)
+        {
+            this.student = student;
+        }
+
+        public int Id
+        {
+            get
+            {
+                return student.Id;
+            }
+            set
+            {
+                student.Id = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return student.FirstName;
+            }
+            set
+            {
+                student.FirstName = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return student.LastName;
+            }
+            set
+            {
+                student.LastName = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return student.Age;
+            }
+            set
+            {
+                student.Age = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        public Gender Gender
+        {
+            get
+            {
+                return student.Gender;
+            }
+            set
+            {
+                student.Gender = value;
+
+                OnPropertyChanged();
+            }
+        }
+    }
+}
