3fae36e0f62d73acdcef6331187cbc49b7d9314f
Vasiliy Shakun <shakun.vasiliy@gmail.com>
2017-08-05 15:27:19 +0300

    Added the StudentService at the APP

---
+++ TestTaskStudents.APP/Services/StudentService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using TestTaskStudents.BLL.Interfaces;
+using TestTaskStudents.BLL.ViewModels;
+using TestTaskStudents.APP.Views;
+
+namespace TestTaskStudents.APP.Services
+{
+    public class StudentService : IStudentService
+    {
+        public StudentViewModel Create()
+        {
+            var createStudentWindow = new EditStudentWindow();
+            var createResult = createStudentWindow.ShowDialog();
+
+            return createResult == true ? (StudentViewModel)createStudentWindow.DataContext : null;
+        }
+
+        public bool Edit(StudentViewModel student)
+        {
+            var editStudentWindow = new EditStudentWindow(student);
+            var editResult = editStudentWindow.ShowDialog();
+
+            return editResult ?? false;
+        }
+    }
+}
