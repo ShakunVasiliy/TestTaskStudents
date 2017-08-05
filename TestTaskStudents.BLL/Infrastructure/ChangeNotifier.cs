5af131b8ebdf7fb5df38e953e9a804c209e4746a
Vasiliy Shakun <shakun.vasiliy@gmail.com>
2017-08-05 12:57:39 +0300

    Added class ChangeNotifier

---
+++ TestTaskStudents.BLL/Infrastructure/ChangeNotifier.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TestTaskStudents.BLL.Infrastructure
+{
+    public abstract class ChangeNotifier: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
