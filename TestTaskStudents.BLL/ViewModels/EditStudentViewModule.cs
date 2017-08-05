a16980d4bf98b74f64fd139dac06e8e29dcfda61
Vasiliy Shakun <shakun.vasiliy@gmail.com>
2017-08-05 14:35:04 +0300

    Added the class EditStudentViewModel at BLL

---
+++ TestTaskStudents.BLL/ViewModels/EditStudentViewModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Data;
+
+using TestTaskStudents.BLL.Infrastructure;
+
+namespace TestTaskStudents.BLL.ViewModels
+{
+    public class EditStudentViewModule
+    {
+        public StudentViewModel Student { get; set; }
+        public IValueConverter GenderConverter { get;}
+
+        public EditStudentViewModule()
+        {
+            Student = new StudentViewModel();
+            GenderConverter = new GenderConverter();
+        }
+    }
+}
