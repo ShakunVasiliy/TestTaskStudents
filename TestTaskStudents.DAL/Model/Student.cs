11d2392a763d2afe70fa0cf7bd221bd7449d3dea
Vasiliy Shakun <shakun.vasiliy@gmail.com>
2017-08-05 12:52:24 +0300

    Added model Student

---
+++ TestTaskStudents.DAL/Model/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using TestTaskStudents.DAL.Infrastructure;
+
+namespace TestTaskStudents.DAL.Model
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public Gender Gender { get; set; }
+    }
+}
