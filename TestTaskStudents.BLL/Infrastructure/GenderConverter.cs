eab6b809a6db1480e0b8bd139abfd557c5518d41
Vasiliy Shakun <shakun.vasiliy@gmail.com>
2017-08-05 14:27:37 +0300

    Added the class GenderConverter at BLL

---
+++ TestTaskStudents.BLL/Infrastructure/GenderConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+using TestTaskStudents.DAL.Infrastructure;
+
+namespace TestTaskStudents.BLL.Infrastructure
+{
+    public class GenderConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((Gender)value).ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Gender gender;
+            var parseResult = Enum.TryParse(value.ToString(), true, out gender);
+
+            return parseResult ? gender : Gender.Man;
+        }
+    }
+}
