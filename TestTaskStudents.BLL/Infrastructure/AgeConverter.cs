8308ae2102312a0658a4aec761ac1412e697fee8
Vasiliy Shakun <shakun.vasiliy@gmail.com>
2017-08-06 16:00:08 +0300

    AgeConverter

---
+++ TestTaskStudents.BLL/Infrastructure/AgeConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace TestTaskStudents.BLL.Infrastructure
+{
+    public class AgeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int age = (int)value;
+            string ageString = age.ToString();
+
+            int remainder = age % 10;
+
+            if ((10 <= age) && (age < 20))
+            {
+                ageString += " лет";
+            }
+            else if (remainder == 1)
+            {
+                ageString += " год";
+            } 
+            else if ((0 < remainder) && (remainder < 5))
+            {
+                ageString += " годa";
+            }
+            else
+            {
+                ageString += " лет";
+            }
+
+            return ageString;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
