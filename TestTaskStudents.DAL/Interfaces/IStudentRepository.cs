7547e4272490b757dfd5844083f8bcef371752ac
Vasiliy Shakun <shakun.vasiliy@gmail.com>
2017-08-06 12:49:40 +0300

    IStudentRepository

---
+++ TestTaskStudents.DAL/Interfaces/IStudentRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TestTaskStudents.DAL.Models;
+
+namespace TestTaskStudents.DAL.Interfaces
+{
+    public interface IStudentRepository : IDisposable
+    {
+        IEnumerable<Student> GetAll();
+        void Add(Student student);
+        void Save();
+    }
+}
